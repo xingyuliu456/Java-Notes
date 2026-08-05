2be5e6743d2b19595ecdcf97d2f8e25a5faf352a
xingyuliu456 <xingyuliu123@qq.com>
2026-08-05 23:39:37 +0800

    更新内存原理代码

---
+++ 01-HelloWorld/src/mechanisim/Test1.java
@@ -0,0 +1,8 @@
+package mechanisim;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int[] arr = new int[5]; // arr是引用数据类型，也存储在栈上面
+        System.out.println(arr); // 打印内存地址
+    }
+}
